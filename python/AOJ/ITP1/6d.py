6d99871e3103f036c35e22af2f4b82464c700a12
patt812 <dev.patt812@gmail.com>
2022-07-10 11:19:03 +0900

    AC ITP1-6C

---
+++ python/AOJ/ITP1/6d.py
@@ -0,0 +1,13 @@
+a, b = []
+n, m = map(int, input().split())
+
+for x in range(n):
+    a.append(list(map(int, input().split())))
+for x in range(m):
+    b.append(int(input()))
+
+for i in range(len(a)):
+    sum = 0
+    for j in range(len(b)):
+        sum += (a[i][j] * b[j])
+    print(sum)
