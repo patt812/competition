e56bdbbd44541e33812d771af5c0bbad9f005a07
patt812 <dev.patt812@gmail.com>
2023-04-02 13:34:23 +0900

    AC ABC123-A

---
+++ python/AtCoder/ABC/123/123a.py
@@ -0,0 +1,25 @@
+a=int(input())
+int(input())
+int(input())
+int(input())
+e=int(input())
+k=int(input())
+
+if e-a > k:
+    print(":(")
+else:
+    print("Yay!")
+
+# a = [int(input
+# a.append(int(inpu())]t()))
+# a.append(int(input()))
+# a.append(int(input()))
+# a.append(int(input()))
+# k = int(input())
+
+# for i in range(len(a)):
+#   for j in range(i+1, len(a)):
+#     if abs(a[i] - a[j]) > k:
+#       print(":(")
+#       exit()
+# print("Yay!")
